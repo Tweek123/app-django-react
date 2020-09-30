550689a39a35f67dc29d3a93621a044c5f06209a
Tweek123 <swapep@mail.com>
2020-09-30 21:35:38 +0300

    deploy

--- DjangoRest/settings.py
+++ DjangoRest/settings.py
@@ -68,6 +68,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
