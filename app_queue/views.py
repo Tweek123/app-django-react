a210c5c67becbff4af7641f2d29cfb1c0471abaf
Tweek123 <swapep@mail.com>
2020-09-30 21:30:17 +0300

    deploy

--- app_queue/views.py
+++ app_queue/views.py
@@ -31,7 +31,7 @@ api_key = 'abcdef12345'
 # Create your views here.
 
 def index(request):
-    return render(request, "build/index.html")
+    return render(request, "index.html")
 
 @csrf_exempt
 def client_requests(request):
